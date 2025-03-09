2a9721dcbe88b8fd3a31bc13a351f663060c1186
thefasquelle <theotime.fasquemlle@outlook.fr>
2025-03-09 23:37:28 +0100

    add code

---
+++ MalverkSrc/BUGlasses.lua
@@ -0,0 +1,52 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: BU Glasses - malverk
+--- MOD_ID: BUGlasses
+--- PREFIX: BUGlasses
+--- MOD_AUTHOR: [Zaales]
+--- MOD_DESCRIPTION: A silly mod to Docify all jokers.
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+-- default texture pack
+AltTexture({
+    key = 'jokers',
+    set = 'Joker',
+    path = 'Jokers.png',
+    loc_txt = {
+        name = 'BU glasses'
+    }
+})
+TexturePack{
+    key = 'buglasses_jokers_pack',
+    textures = {
+        'BUGlasses_jokers'
+    },
+    loc_txt = {
+        name = 'BU glasses',
+        text = {
+            'Give Doc\'s glasses to all jokers'
+        }
+    }
+}
+
+-- light texture pack
+AltTexture({
+    key = 'jokers_light',
+    set = 'Joker',
+    path = 'Jokers_light_edges.png',
+    loc_txt = {
+        name = 'BU glasses - light'
+    }
+})
+TexturePack{
+    key = 'buglasses_jokers_pack_light',
+    textures = {
+        'BUGlasses_jokers_light'
+    },
+    loc_txt = {
+        name = 'BU glasses - light',
+        text = {
+            'Give Doc\'s glasses to all jokers.\nThis version has less black edges'
+        }
+    }
+}
